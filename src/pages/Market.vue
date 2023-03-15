80b7b9a9679ad88b5eb5698337e0791753de5138
RichardZhiLi <948945081@qq.com>
2023-03-15 22:36:23 +0800

    1.添加了utils文件夹，里面包含了ajax.js文件，封装了ajax以便于后续请求api. 2.添加了请求主页数据API的相关逻辑. 3.暂时性去除掉了挂单率这一个filed（后端还未开发完成）

--- src/pages/Market.vue
+++ src/pages/Market.vue
@@ -22,10 +22,9 @@
           </v-row>
           <v-row align="center">
             <v-col style="max-width:100vw;overflow:scroll">
-              <v-btn-toggle model-value='currentTab' v-model='currentTab'>
+              <v-btn-toggle model-value='currentTab' v-model='currentTab' mandatory>
                 <v-btn v-for="(item,index) in tabItems" :key="index" :value='item'>
                   {{item}}
-
                 </v-btn>
               </v-btn-toggle>
             </v-col>
@@ -39,18 +38,18 @@
                 <th class="text-center text-no-wrap">总成交额<v-btn icon style="zoom:0.5" @click="sort"><v-icon>{{this.ascendSort?'mdi-arrow-expand-up':'mdi-arrow-expand-down'}}</v-icon></v-btn></th>
                 <th class="text-center">订单数</th>
                 <th class="text-center">最低价</th>
-                <th class="text-center">挂单率</th>
+                <!-- <th class="text-center">挂单率</th> -->
               </tr>
             </thead>
             <tbody>
-              <tr v-for="item in transactions" :key="item.name" class="text-center"
+              <tr v-for="item in transactions" :key="item.commodityid" class="text-center"
               @click='navigateTo'
               >
-                <td>{{ item.IP }}</td>
-                <td>{{ item.totalValue }}</td>
-                <td>{{item.orderNum}}</td>
-                <td>{{item.minimumValue}}</td>
-                <td>{{item.rate}}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.total_amt }}</td>
+                <td>{{item.orders}}</td>
+                <td>{{item.min_amt}}</td>
+                <!-- <td>{{item.rate}}</td> -->
               </tr>
             </tbody>
           </v-simple-table>
@@ -58,74 +57,34 @@
       </v-container>
 </template>
 <script>
+import {request} from '../utils/ajax'
+const tabMap = new Map([
+  ['5分钟',300],
+  ['10分钟',600],
+  ['1小时',3600],
+  ['4小时',14400],
+  ['1天',86400]
+]) //负责映射每一个tab对应的秒数
 export default {
-    // eslint-disable-next-line vue/multi-word-component-names
-    name:'Market',
     data: () => ({
     ascendSort:true,
     selectItems: ["唯一艺术"],
     defaultSelect: "唯一艺术",
     tabItems: ["5分钟", "10分钟", "1小时", "4小时", "1天"],
-    currentTab:'5分钟',
-    transactions: [
-      {
-        IP:"192.168.1.0",
-        totalValue:100,
-        orderNum:10,
-        minimumValue:100,
-        rate:"30%"
-      },
-      {
-        IP:"192.168.1.1",
-        totalValue:101,
-        orderNum:20,
-        minimumValue:100,
-        rate:"15%"
-      },
-      {
-        IP:"192.168.1.1",
-        totalValue:102,
-        orderNum:20,
-        minimumValue:100,
-        rate:"15%"
-      },
-      {
-        IP:"192.168.1.1",
-        totalValue:103,
-        orderNum:20,
-        minimumValue:100,
-        rate:"15%"
-      },
-      {
-        IP:"192.168.1.1",
-        totalValue:104,
-        orderNum:20,
-        minimumValue:100,
-        rate:"15%"
-      },
-      {
-        IP:"192.168.1.1",
-        totalValue:105,
-        orderNum:20,
-        minimumValue:100,
-        rate:"15%"
-      },
-      {
-        IP:"192.168.1.1",
-        totalValue:106,
-        orderNum:20,
-        minimumValue:100,
-        rate:"15%"
-      },
-      {
-        IP:"192.168.1.1",
-        totalValue:107,
-        orderNum:20,
-        minimumValue:100,
-        rate:"15%"
-      }
-    ],
+    currentTab:'1天',
+    clientHeight:0,
+    scrollHeight:0,
+    transactions: []
   }),
+  watch:{
+    //侦测每一次的tab变动，一旦触发即向后端发送API请求
+    currentTab:function(newV){
+        this.getTransactions(tabMap.get(newV))
+    }
+  },
+  mounted:function(){
+    this.getTransactions(tabMap.get('1天')) //页面加载时默认获取1天前的交易记录
+  },
   methods:{
     navigateTo:function(){
         const {$router} = this;
@@ -135,9 +94,34 @@ export default {
         this.ascendSort = !this.ascendSort
         const {transactions,ascendSort} = this;
         let sorted = null;
-        if(ascendSort) sorted = transactions.sort((a,b)=>a.totalValue-b.totalValue)
-        else sorted = transactions.sort((a,b)=>b.totalValue - a.totalValue)
+        if(ascendSort) sorted = transactions.sort((a,b)=>a.total_amt-b.total_amt)
+        else sorted = transactions.sort((a,b)=>b.total_amt - a.total_amt)
         this.transactions  = sorted
+    },
+    // tableScroll:function(e){
+    // 分页功能，待完成
+    //   const {scrollTop} = e.target;
+    //   console.log(this.clientHeight,scrollTop,this.scrollHeight)
+    //   if(scrollTop + this.clientHeight >= this.scrollHeight){
+    //     console.log('trigger')
+    //   }
+    // },
+    getTransactions:function(tabItemInSeconds){
+        //接收传入的tab所对应的秒数，例如‘5分钟’对应300秒，然后请求API获取对应数据
+        const endDate = Date.now() //当前的时间戳，是以毫秒为单位
+        const startDate = endDate - tabItemInSeconds * 1000 //用当前的毫秒数减去tab所表示的毫秒数，就是查询多少分钟前的交易记录
+        const parameter = {
+          startDate,
+          endDate
+        }
+        request('post','/api/bwt/theOne/data',parameter).then(resolve=>{
+          this.transactions = JSON.parse(resolve).data
+          this.ascendSort = true //由于每次切换tab默认数据都是按照降序排序，因此这里要将ascendSort这个flag给默认设置为true
+          this.sort()
+          }).catch(err=>{
+          console.error('获取交易数据失败,错误为', err)
+          })
+    
     }
   }
 }
---
+++ src/utils/ajax.js
@@ -0,0 +1,17 @@
+const defaultAPIDomain = 'http://api.nftbwc.com' //默认的API访问域名
+const request = function(method = 'get',api,parameter){
+    const url = defaultAPIDomain + api
+    return new Promise ((resolve,reject)=>{
+        const xhr = new XMLHttpRequest();
+        xhr.open(method,url)
+        xhr.setRequestHeader('Content-Type','application/json')
+        xhr.send(JSON.stringify(parameter))
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200) resolve(xhr.response)
+                else reject(xhr.status)
+            }
+        }
+    })
+}
+export {request}
